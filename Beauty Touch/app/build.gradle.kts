ded997c32545c4b5ba156137fe61f04c8d848627
selimdawa <selimdawa@gmail.com>
2026-06-09 00:13:51 +0300

    Glide Update

--- Beauty Touch/app/build.gradle.kts
+++ Beauty Touch/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.services)
-    id("kotlin-kapt")
+    alias(libs.plugins.google.ksp)
 }
 
 android {
@@ -17,30 +17,38 @@ android {
         applicationId = "com.flatcode.beautytouch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.15"
+        versionCode = 5
+        versionName = "1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    signingConfigs {
-        create("release") {
-            storeFile = file("D:\\MyProjects\\Kotlin\\BeautyTouch\\Beauty Touch\\BeautyTouch.jks")
-            storePassword = "00000000"
-            keyAlias = "BeautyTouch"
-            keyPassword = "00000000"
-        }
-    }
+    //signingConfigs {
+    //    create("release") {
+    //        storeFile = file("D:\\MyProjects\\Kotlin\\BeautyTouch\\Beauty Touch\\BeautyTouch.jks")
+    //        storePassword = "00000000"
+    //        keyAlias = "BeautyTouch"
+    //        keyPassword = "00000000"
+    //    }
+    //}
     buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            isShrinkResources = true
+        release {
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
+    //buildTypes {
+    //    getByName("release") {
+    //        signingConfig = signingConfigs.getByName("release")
+    //        isMinifyEnabled = true
+    //        isShrinkResources = true
+    //        proguardFiles(
+    //            getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+    //        )
+    //    }
+    //}
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -53,6 +61,7 @@ android {
 
     buildFeatures {
         dataBinding = true
+        buildConfig = true
     }
 }
 
@@ -79,8 +88,7 @@ dependencies {
     //Image
     implementation(libs.circleimageview)                //Circle Image
     implementation(libs.glide)                          //Glide Image
-    //noinspection KaptUsageInsteadOfKsp
-    kapt(libs.compiler)                                 //Glide Compiler
+    ksp(libs.glide.ksp)                                 //Glide Compiler
     implementation(libs.material.ripple)                //Ripple Effect
     api(libs.android.image.cropper)                     //Image Crop
     implementation(libs.autoimageslider)                //Slider Show
