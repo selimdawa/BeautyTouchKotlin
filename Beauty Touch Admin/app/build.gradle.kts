076f391091b5f83993b179047489e83b2bf36a9a
selimdawa <selimdawa@gmail.com>
2026-06-09 02:45:13 +0300

    SDK 37 / Android 17

--- Beauty Touch Admin/app/build.gradle.kts
+++ Beauty Touch Admin/app/build.gradle.kts
@@ -8,15 +8,13 @@ plugins {
 android {
     namespace = "com.flatcode.beautytouchadmin"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.flatcode.beautytouchadmin"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 5
         versionName = "1.30"
 
